c78f8a9eae94052eb0d8225fbd8909a2d9242f8c
Owen <opster15@gmail.com>
2021-11-21 01:23:37 +0000

    Enemy Health added

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int currentHealth,maxHealth;
+
+    public Animator anim;
+
+    public void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+
+    void Update()
+    {
+        /*
+        if (inputManager.spacePressed)
+        {
+            TakeDamage(150);
+        }
+        */
+        if (currentHealth <= 0)
+        {
+            Debug.Log("DIE");
+            anim.SetBool("IsDead", true);
+        }
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
---
+++ Assets/Scripts/Player/Combat/AttackScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+
+    public GameObject hitPoint,hitEffect;
+
+    public LayerMask whatIsEnemies;
+
+    public int damage;
+    public float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Collider[] enemies = Physics.OverlapSphere(hitPoint.transform.position, range, whatIsEnemies);
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].GetComponent<EnemyHealth>().TakeDamage(damage);
+                GameObject hit = (GameObject)Instantiate(hitEffect, transform.position, Quaternion.identity);
+                Destroy(hit, 1.0f);
+            }
+        }
+    }
+}
