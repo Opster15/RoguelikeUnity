3f2969f80034bc3f68891196dcb67f55b2b7501c
Owen <opster15@gmail.com>
2021-11-29 23:36:16 +0000

    AbilityUpdates

--- Assets/Scripts/Abilities/AbilityHolder.cs
+++ Assets/Scripts/Abilities/AbilityHolder.cs
@@ -5,55 +5,212 @@ using UnityEngine;
 public class AbilityHolder : MonoBehaviour
 {
 
-    public Ability ability;
+    public Ability[] abilities;
     public AbilityUI ui;
-    float cooldownTime,activeTime;
+    public float[] cooldownTime;
+    public float[] activeTime;
 
-    public enum AbilityState
+    #region Ability Enums   
+
+    public enum Ability1State
     {
         ready,
         active,
         cooldown
     }
+    public enum Ability2State
+    {
+        ready,
+        active,
+        cooldown
+    }
+    public enum Ability3State
+    {
+        ready,
+        active,
+        cooldown
+    }
+    public enum Ability4State
+    {
+        ready,
+        active,
+        cooldown
+    }
+    #endregion
 
-    AbilityState state = AbilityState.ready;
+    Ability1State state1 = Ability1State.ready;
+    Ability2State state2 = Ability2State.ready;
+    Ability3State state3 = Ability3State.ready;
+    Ability4State state4 = Ability4State.ready;
 
-    public KeyCode key;
 
     private void Update()
     {
-        switch (state)
+        Ability1();
+        Ability2();
+        Ability3();
+        Ability4();
+    }
+
+    void Ability1()
+    {
+        #region ability1
+        switch (state1)
         {
-            case AbilityState.ready:
-                if (Input.GetKeyDown(key))
+            case Ability1State.ready:
+                if (Input.GetKeyDown(KeyCode.Mouse0))
                 {
-                    ability.Activate(gameObject);
-                    state = AbilityState.active;
-                    activeTime = ability.activeTime;
+                    abilities[0].Activate(gameObject);
+                    state1 = Ability1State.active;
+                    activeTime[0] = abilities[0].activeTime;
                 }
                 return;
-            case AbilityState.active:
-                if(activeTime > 0)
+            case Ability1State.active:
+                if (activeTime[0] > 0)
                 {
-                    activeTime -= Time.deltaTime;
+                    activeTime[0] -= Time.deltaTime;
                 }
                 else
                 {
-                    state = AbilityState.cooldown;
-                    cooldownTime = ability.cooldownTime;
-                    ui.abilityInput = true;
+                    state1 = Ability1State.cooldown;
+                    cooldownTime[0] = abilities[0].cooldownTime;
+                    ui.abilityInput[0] = true;
                 }
                 return;
-            case AbilityState.cooldown:
-                if (cooldownTime > 0)
+            case Ability1State.cooldown:
+                if (cooldownTime[0] > 0)
                 {
-                    cooldownTime -= Time.deltaTime;
+                    cooldownTime[0] -= Time.deltaTime;
                 }
                 else
                 {
-                    state = AbilityState.ready;
+                    state1 = Ability1State.ready;
                 }
                 return;
+                
         }
+        #endregion
+    }
+
+    void Ability2()
+    {
+        #region ability2
+        switch (state2)
+        {
+            case Ability2State.ready:
+                if (Input.GetKeyDown(KeyCode.Mouse1))
+                {
+                    abilities[1].Activate(gameObject);
+                    state2 = Ability2State.active;
+                    activeTime[1] = abilities[1].activeTime;
+                }
+                return;
+            case Ability2State.active:
+                if (activeTime[1] > 0)
+                {
+                    activeTime[1] -= Time.deltaTime;
+                }
+                else
+                {
+                    state1 = Ability1State.cooldown;
+                    cooldownTime[1] = abilities[1].cooldownTime;
+                    ui.abilityInput[1] = true;
+                }
+                return;
+            case Ability2State.cooldown:
+                if (cooldownTime[1] > 0)
+                {
+                    cooldownTime[1] -= Time.deltaTime;
+                }
+                else
+                {
+                    state2 = Ability2State.ready;
+                }
+                return;
+
+        }
+        #endregion
+    }
+    void Ability3()
+    {
+        #region ability3
+        switch (state3)
+        {
+
+
+            case Ability3State.ready:
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    abilities[2].Activate(gameObject);
+                    state3 = Ability3State.active;
+                    activeTime[2] = abilities[2].activeTime;
+                }
+                return;
+            case Ability3State.active:
+                if (activeTime[2] > 0)
+                {
+                    activeTime[2] -= Time.deltaTime;
+                }
+                else
+                {
+                    state3 = Ability3State.cooldown;
+                    cooldownTime[2] = abilities[2].cooldownTime;
+                    ui.abilityInput[2] = true;
+                }
+                return;
+            case Ability3State.cooldown:
+                if (cooldownTime[2] > 0)
+                {
+                    cooldownTime[2] -= Time.deltaTime;
+                }
+                else
+                {
+                    state3 = Ability3State.ready;
+                }
+                return;
+
+        }
+        #endregion
+    }
+    void Ability4()
+    {
+        #region ability4
+        switch (state4)
+        {
+
+
+            case Ability4State.ready:
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    abilities[3].Activate(gameObject);
+                    state4 = Ability4State.active;
+                    activeTime[3] = abilities[3].activeTime;
+                }
+                return;
+            case Ability4State.active:
+                if (activeTime[3] > 0)
+                {
+                    activeTime[3] -= Time.deltaTime;
+                }
+                else
+                {
+                    state4 = Ability4State.cooldown;
+                    cooldownTime[3] = abilities[3].cooldownTime;
+                    ui.abilityInput[3] = true;
+                }
+                return;
+            case Ability4State.cooldown:
+                if (cooldownTime[3] > 0)
+                {
+                    cooldownTime[3] -= Time.deltaTime;
+                }
+                else
+                {
+                    state4 = Ability4State.ready;
+                }
+                return;
+
+        }
+        #endregion
     }
 }
--- Assets/Scripts/UI/AbilityUI.cs
+++ Assets/Scripts/UI/AbilityUI.cs
@@ -1,51 +1,141 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 public class AbilityUI : MonoBehaviour
 {
     public GameObject player;
-    Ability ability;
+    public Ability[] abilities;
     AbilityHolder abilityHolder;
-    public float cooldown1;
-    public bool abilityInput = false;
-    public bool isCooldown = false;
+    public float[] cooldowns;
+    public bool[] abilityInput;
+    public bool[] isCooldown;
 
-    public Image abilityImage1;
+    public Image[] abilityImages;
 
 
     private void Awake()
     {
-        ability = player.GetComponent<AbilityHolder>().ability;
         abilityHolder = player.GetComponent<AbilityHolder>();
-        cooldown1 = ability.cooldownTime;
-        abilityImage1.fillAmount = 0;
+        abilities[0] = player.GetComponent<AbilityHolder>().abilities[0];
+        abilities[1] = player.GetComponent<AbilityHolder>().abilities[1];
+        abilities[2] = player.GetComponent<AbilityHolder>().abilities[2];
+        abilities[3] = player.GetComponent<AbilityHolder>().abilities[3];
+        
+        cooldowns[0] = abilities[0].cooldownTime;
+        cooldowns[1] = abilities[1].cooldownTime;
+        cooldowns[2] = abilities[2].cooldownTime;
+        cooldowns[3] = abilities[3].cooldownTime;
+
+        abilityImages[0].fillAmount = 0;
+        abilityImages[1].fillAmount = 0;
+        abilityImages[2].fillAmount = 0;
+        abilityImages[3].fillAmount = 0;
+
+        abilityInput[0] = false;
+        abilityInput[1] = false;
+        abilityInput[2] = false;
+        abilityInput[3] = false;
+
+        isCooldown[0] = false;
+        isCooldown[1] = false;
+        isCooldown[2] = false;
+        isCooldown[3] = false;
     }
 
     public void Update()
     {
-        if(abilityInput == true && isCooldown == false)
+        if(abilityInput[0] == true && isCooldown[0] == false)
         {
-            abilityImage1.fillAmount = 1;
-            isCooldown = true;
+            abilityImages[0].fillAmount = 1;
+            isCooldown[0] = true;
+        }
+        if (abilityInput[1] == true && isCooldown[1] == false)
+        {
+            abilityImages[1].fillAmount = 1;
+            isCooldown[1] = true;
+        }
+        if (abilityInput[2] == true && isCooldown[2] == false)
+        {
+            abilityImages[2].fillAmount = 1;
+            isCooldown[2] = true;
+        }
+        if (abilityInput[3] == true && isCooldown[3] == false)
+        {
+            abilityImages[3].fillAmount = 1;
+            isCooldown[3] = true;
         }
 
+
+
         Ability1();
+        Ability2();
+        Ability3();
+        Ability4();
     }
 
     void Ability1()
     {
 
 
-        if (isCooldown == true)
+        if (isCooldown[0] == true)
         {
-            abilityImage1.fillAmount -= 1 / cooldown1 * Time.deltaTime;
+            abilityImages[0].fillAmount -= 1 / cooldowns[0] * Time.deltaTime;
 
-            if(abilityImage1.fillAmount <= 0)
+            if(abilityImages[0].fillAmount <= 0)
             {
-                abilityImage1.fillAmount = 0;
-                abilityInput = false;
-                isCooldown = false;
+                abilityImages[0].fillAmount = 0;
+                abilityInput[0] = false;
+                isCooldown[0] = false;
+            }
+        }
+
+    }
+    void Ability2()
+    {
+
+
+        if (isCooldown[1] == true)
+        {
+            abilityImages[1].fillAmount -= 1 / cooldowns[1] * Time.deltaTime;
+
+            if (abilityImages[1].fillAmount <= 0)
+            {
+                abilityImages[1].fillAmount = 0;
+                abilityInput[1] = false;
+                isCooldown[1] = false;
+            }
+        }
+
+    }
+    void Ability3()
+    {
+
+
+        if (isCooldown[2] == true)
+        {
+            abilityImages[2].fillAmount -= 1 / cooldowns[2] * Time.deltaTime;
+
+            if (abilityImages[2].fillAmount <= 0)
+            {
+                abilityImages[2].fillAmount = 0;
+                abilityInput[2] = false;
+                isCooldown[2] = false;
+            }
+        }
+
+    }
+    void Ability4()
+    {
+
+
+        if (isCooldown[3] == true)
+        {
+            abilityImages[3].fillAmount -= 1 / cooldowns[3] * Time.deltaTime;
+
+            if (abilityImages[3].fillAmount <= 0)
+            {
+                abilityImages[3].fillAmount = 0;
+                abilityInput[3] = false;
+                isCooldown[3] = false;
             }
         }
 
